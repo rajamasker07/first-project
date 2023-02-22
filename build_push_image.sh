0fcff4ddb3f804490b451654f26a6797b691f694
Abdur Rosyid Ridwan <rosyidohyeah@gmail.com>
2023-02-22 20:30:33 +0900

    update script

--- build_push_image.sh
+++ build_push_image.sh
@@ -3,3 +3,9 @@
 docker build --tag item-app:v1 .
 
 docker images
+
+docker tag item-app:v1 rosyid/item-app:v1
+
+echo $PASSWORD_DOCKER_HUB | docker login -u rosyid --password-stdin
+
+docker push rosyid/item-app:v1
